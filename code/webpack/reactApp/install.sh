378b7d7bf64984197efc3d4e4130ebf829763fa6
xiaofeng <xlics05@gmail.com>
2017-03-21 16:14:13 -0400

    folder adjustment

---
+++ code/webpack/reactApp/install.sh
@@ -0,0 +1,12 @@
+#TODO check NPM and NODEJS version
+npm init
+npm install -g babel
+npm install -g babel-cli
+npm install webpack --save
+npm install webpack-dev-server --save
+npm install react --save
+npm install react-dom --save
+npm install babel-core
+npm install babel-loader
+npm install babel-preset-react
+npm install babel-preset-es2015
