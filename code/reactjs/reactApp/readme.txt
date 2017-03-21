7ff13c59bb0a7df20bc8f0f10c8e3fc94eb5dbea
xiaofeng <xlics05@gmail.com>
2017-03-21 16:54:57 -0400

    update react framework tutorial

---
+++ code/reactjs/reactApp/readme.txt
@@ -0,0 +1,3 @@
+run: webpack 
+in the directory that contains webpack.config.js will 
+generate index.js
--- code/reactjs/reactApp/webpack.config.js
+++ code/reactjs/reactApp/webpack.config.js
@@ -1,23 +1,24 @@
 var config = {
-   entry: '/Users/xiaofeng/reactjs/reactApp/main.js',
-	
+   context: __dirname,
+   entry:'/Users/xiaofeng/tutorial/code/reactjs/reactApp/main.js',
+
    output: {
-      path:'/Users/xiaofeng/reactjs/reactApp',
+      path:__dirname,
       filename: 'index.js',
    },
-	
+
    devServer: {
       inline: true,
       port: 8081
    },
-	
+
    module: {
       loaders: [
          {
             test: /\.jsx?$/,
             exclude: /node_modules/,
             loader: 'babel-loader',
-				
+
             query: {
                presets: ['es2015', 'react']
             }
