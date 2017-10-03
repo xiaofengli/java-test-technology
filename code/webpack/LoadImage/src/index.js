47a13a560fc13cab4067aa499d0881441e993ad3
xiaofeng.li@icims.com <xiaofeng.li@icims.com>
2017-10-03 17:35:38 -0400

    new examples

---
+++ code/webpack/LoadImage/src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import Icon from './icon.png';
+
+function component() {
+    var element = document.createElement('div');
+   // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add(".fontColor");
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+    return element;
+}
+
+document.body.appendChild(component());
